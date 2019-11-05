e680e53fae5f76ba23f3b488990b34051850cad5
Matthias Bethke <matthias@towiski.de>
2019-11-06 00:05:17 +0530

    allow passing playbook argument

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ if [ -z "$ROOTPW" ]; then
     echo >&2 "ROOTPW" is unset
     exit 1
 fi
-OSMHOST="$(sed -n 's/^- hosts: \(.*\)/\1/p' < playbook-osm.yaml)"
-ansible-playbook -i$OSMHOST, playbook-osm.yaml --extra-vars "rootpwd=$(mkpasswd -m sha-512 "$ROOTPW")"
+PLAYBOOK="${1:-playbook-osm.yaml}"
+OSMHOST="$(sed -n 's/^- hosts: \(.*\)/\1/p' < "$PLAYBOOK")"
+ansible-playbook -i$OSMHOST, "$PLAYBOOK" --extra-vars "rootpwd=$(mkpasswd -m sha-512 "$ROOTPW")"
