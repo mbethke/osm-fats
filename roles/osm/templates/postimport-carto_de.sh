ea3ba2d6ee7bf3ddee9ee53ee1491fac2a6b4d33
Matthias Bethke <matthias@towiski.de>
2019-11-05 14:31:13 +0530

    fix path

--- roles/osm/templates/postimport-carto_de.sh
+++ roles/osm/templates/postimport-carto_de.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-cd /etc/mapnik-osm-data/carto
+cd /etc/mapnik-osm-data/carto-de
 
 psql -d"gis" -ef indexes-hstore.sql
 views_osmde/apply-views.sh {{ gis_dbname }} {{ style.default_language }}
